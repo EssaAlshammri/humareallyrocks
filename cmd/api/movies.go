d2eb5dbb8e3a8b6db6af239401003e2c17e068d1
EssaAlshammri <essa.alshammri@gmail.com>
2024-05-09 20:05:34 +0300

    populate data

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"time"
 )
 
 func (app application) createMovieHandler(ctx context.Context, input *MovieCreateInput) (*struct{}, error) {
@@ -9,7 +10,10 @@ func (app application) createMovieHandler(ctx context.Context, input *MovieCreat
 }
 
 func (app application) getMovieHandler(ctx context.Context, input *MovieGetInput) (*MovieGetOutput, error) {
-	// id := input.ID
+	id := input.ID
 	resp := &MovieGetOutput{}
+	resp.Body.ID = id
+	resp.Body.Title = "hello"
+	resp.Body.CreatedAt = time.Now().UTC()
 	return resp, nil
 }
--- cmd/api/schema.go
+++ cmd/api/schema.go
@@ -22,7 +22,7 @@ type MovieGetInput struct {
 
 type MovieGetOutput struct {
 	Body struct {
-		ID        int64     `json:"ID" doc:"Unique integer ID for the movie"`
+		ID        uint     `json:"ID" doc:"Unique integer ID for the movie"`
 		CreatedAt time.Time `json:"createdAt" doc:"Timestamp for when the movie is added to our database"`
 		Title     string    `json:"title" doc:"Movie title"`
 		Year      int32     `json:"year" doc:"Movie release year"`

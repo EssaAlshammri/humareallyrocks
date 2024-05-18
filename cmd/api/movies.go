2c15325832577ee3587fb18f4bcf61c89af78a05
EssaAlshammri <essa.alshammri@gmail.com>
2024-05-18 11:41:22 +0300

    complete create handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,10 +3,25 @@ package main
 import (
 	"context"
 	"time"
+
+	"github.com/EssaAlshammri/humareallyrocks/internal/data"
 )
 
-func (app application) createMovieHandler(ctx context.Context, input *MovieCreateInput) (*struct{}, error) {
-	return nil, nil
+func (app application) createMovieHandler(ctx context.Context, input *MovieCreateInput) (*MovieCreateOut, error) {
+	movie := &data.Movie{
+		Title:   input.Body.Title,
+		Year:    input.Body.Year,
+		Runtime: input.Body.Runtime,
+		Genres:  input.Body.Genres,
+	}
+	err := app.models.Movies.Insert(movie)
+	if err != nil {
+		return nil, err
+	}
+	output := &MovieCreateOut{}
+	output.Body.ID = movie.ID
+	output.Body.CreatedAt = movie.CreatedAt
+	return output, nil
 }
 
 func (app application) getMovieHandler(ctx context.Context, input *MovieGetInput) (*MovieGetOutput, error) {

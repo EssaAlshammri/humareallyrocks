ec1dbae89dd105bef20345112e800d12aa18be2a
EssaAlshammri <essa.alshammri@gmail.com>
2024-05-18 11:40:35 +0300

    add create in and out struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -14,6 +14,18 @@ type Movie struct {
 	Genres    []string  `json:"genres" doc:"genres for the movie"`
 }
 
+type MovieOut struct {
+	ID        uint      `json:"ID" doc:"Unique integer ID for the movie"`
+	CreatedAt time.Time `json:"createdAt" doc:"Timestamp for when the movie is added to our database"`
+}
+
+type MovieIn struct {
+	Title     string    `json:"title" doc:"Movie title"`
+	Year      int32     `json:"year" doc:"Movie release year"`
+	Runtime   int32     `json:"runtime" doc:"Movie runtime (in minutes)"`
+	Genres    []string  `json:"genres" doc:"genres for the movie"`
+}
+
 type MovieModel struct {
 	DB *sql.DB
 }
